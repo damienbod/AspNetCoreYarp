6ae51e63c73360a2ea590451ffc9783a2af265b4
Damien <damien_bod@hotmail.com>
2021-01-07 17:37:47 +0100

    Adding a demo API

---
+++ LegacyApi/Controllers/CrazyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace LegacyApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CrazyController : ControllerBase
+    {
+        private readonly ILogger<CrazyController> _logger;
+
+        public CrazyController(ILogger<CrazyController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new List<string> { "crazy data from API which cannot be updated", "not public" };
+        }
+    }
+}
