2cb53c7af13f13cba74a350e744db4b09c368466
damienbod <damien_bod@hotmail.com>
2023-02-19 08:35:38 +0100

    Update C# styles

--- AspNetCoreProxy/Startup.cs
+++ AspNetCoreProxy/Startup.cs
@@ -1,7 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Identity.Web;
 using System.IdentityModel.Tokens.Jwt;
 
